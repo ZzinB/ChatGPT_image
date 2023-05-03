97b8d6eeeabb2f9ca95417f6f122862163fca6a1
16shinbi <21600510@handong.edu>
2023-05-03 18:04:26 +0900

    clickEvent

---
+++ script.js
@@ -0,0 +1,6 @@
+const generateBtn = document.querySelector('#generateBtn')
+
+generateBtn.addEventListener('click', () => { //무언가를 하겠다! (클릭 이벤트가 발생)
+    console.log('버튼을 누르면 실행되지롱'); //개발자도구 (cmd + opt + j)
+    
+})
