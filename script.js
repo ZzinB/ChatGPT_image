a988c772e0f49177da9e638a002d8e5a103636af
16shinbi <21600510@handong.edu>
2023-05-06 12:23:18 +0900

    update

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ const generateBtn = document.querySelector('#generateBtn')
 const gridConrainer = document.querySelector('#gridContainer')
 
 generateBtn.addEventListener('click', () => {
+
+    //이미지가 30개 이상힐 경우 모든 이미지 삭제
+    if (gridConrainer.children.length >= 30){   
+        const confirmed = confirm("이미지가 30개 찼습니다. 모든 사진을 지우시겠습니까?");
+        if (confirmed){
+            gridConrainer.innerHTML = ""; //이미지 모두 삭제
+        }
+    }
+
     const randomNumber = Math.floor(Math.random() * 1000) + 1 //1부터 1000사이 랜덤
     const imgUrl = `https://picsum.photos/500?random=${randomNumber}`;//랜덤 이미지 URL 할당
 
     const img = document.createElement('img') //img 태그 생성
     img.src = imgUrl;
+
     gridConrainer.appendChild(img); //이미지를 그리드 컨테이너에 추가
 })
